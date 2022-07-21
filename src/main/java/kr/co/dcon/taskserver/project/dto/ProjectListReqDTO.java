515177577a028eb7c9bb62b24d21e0a0401b4a12
박수석 <suseokpark@dc-on.co.kr>
2022-07-21 13:16:26 +0900

    filter

--- src/main/java/kr/co/dcon/taskserver/project/dto/ProjectListReqDTO.java
+++ src/main/java/kr/co/dcon/taskserver/project/dto/ProjectListReqDTO.java
@@ -15,6 +15,10 @@ public class ProjectListReqDTO implements Forwardable {
     @ApiModelProperty(value = "taskStatus", notes = "taskStatus", example = "open", required = true)
     private String taskStatus;
 
+    @ApiModelProperty(value = "작업자,생성자,수정자 구분", notes = "작업자,생성자,수정자 구분", example = "assign_id")
+    private String issueSearchCode;
+    @ApiModelProperty(value = "작업자,생성자,수정자", notes = "작업자,생성자,수정자", example = "6ef8ef43-428c-44a6-9bf3-9e57d90d6610")
+    private String issueSearchUser;
     @Override
     public String getUrlToForward(String baseUrl) {
         StringBuilder urlParam = new StringBuilder();
@@ -23,6 +27,8 @@ public class ProjectListReqDTO implements Forwardable {
         urlParam.append(projectId);
         urlParam.append("?projectId=").append(projectId);
         urlParam.append("&taskStatus=").append(taskStatus);
+        urlParam.append("&issueSearchCode=").append(issueSearchCode);
+        urlParam.append("&issueSearchUser=").append(issueSearchUser);
 
         return urlParam.toString();
     }
