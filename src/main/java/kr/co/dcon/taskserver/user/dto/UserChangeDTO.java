79ef52fdda98f8e020640910125bd64f0b44a036
jijung-dcon <ji.jung@dc-on.co.kr>
2022-08-04 13:33:27 +0900

    DB 정보 암호화

--- src/main/java/kr/co/dcon/taskserver/user/dto/UserChangeDTO.java
+++ src/main/java/kr/co/dcon/taskserver/user/dto/UserChangeDTO.java
@@ -9,34 +9,17 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class UserChangeDTO {
 
-	@ApiModelProperty(value = "비밀번호", notes = "비밀번호", example = "abc123")
-	private String password;
+    @ApiModelProperty(value = "이메일", notes = "이메일", example = "ripple31222@naver.com", required = true)
+    @NotBlank
+    @Email(message = "이메일 형식에 맞지 않습니다.")
+    private String userEmail;
 
-	@ApiModelProperty(value = "이메일", notes = "이메일", example = "ripple31222@naver.com", required = true)
-	@NotBlank
-	@Email(message = "이메일 형식에 맞지 않습니다.")
-	private String userEmail;
+    @ApiModelProperty(value = "userLocale", notes = "userLocale", example = "ko", required = true)
+    private String userLocale;
 
-//	@ApiModelProperty(value = "전화번호")
-//	@Pattern(regexp = "^(?:\\d\\s?){9,20}$", message = "10 ~ 11 자리의 숫자만 입력 가능합니다.")
-//	private String telNo;
-
-	@ApiModelProperty(value = "result")
-	private String result;
-
-
-	@ApiModelProperty(value = "firstName", notes = "firstName", example = "박", required = true)
-	private String firstName;
-	
-	@ApiModelProperty(value = "lastName", notes = "lastName", example = "테스트", required = true)
-	private String lastName;
-
-	@ApiModelProperty(value = "userLocale", notes = "userLocale", example = "ko", required = true)
-	private String userLocale;
-
-	@ApiModelProperty(value = "userAuth", notes = "userAuth", example = "admin", required = true)
-	private String userAuth;
-	private String useYn;
+    @ApiModelProperty(value = "userAuth", notes = "userAuth", example = "admin", required = true)
+    private String userAuth;
+    private String useYn;
 }
 
 
