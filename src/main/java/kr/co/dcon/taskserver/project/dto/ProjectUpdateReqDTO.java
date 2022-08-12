35968021d8c60e69f811605b18834cf8fd94c063
jijung-dcon <ji.jung@dc-on.co.kr>
2022-08-12 16:58:42 +0900

    프로젝트 수정페이지 수정

--- src/main/java/kr/co/dcon/taskserver/project/dto/ProjectUpdateReqDTO.java
+++ src/main/java/kr/co/dcon/taskserver/project/dto/ProjectUpdateReqDTO.java
@@ -12,8 +12,11 @@ public class ProjectUpdateReqDTO {
     @ApiModelProperty(value = "projectId", notes = "projectId", example = "dcon_project_1", required = true)
     private String projectId;
 
-    @ApiModelProperty(value = "userInfo", notes = "userInfo", example = "task_1", required = false)
+    @ApiModelProperty(value = "userList", notes = "userList", example = "task_1", required = false)
     private List<UserInfoDTO> userList;
+
+    @ApiModelProperty(value = "taskAssignUpdateList", notes = "deleteUserList", example = "task_1", required = false)
+    private List<UserInfoDTO> taskAssignUpdateList;
 
     @ApiModelProperty(value = "updateId", notes = "updateId", example = "6ef8ef43-428c-44a6-9bf3-9e57d90d6610", required = false)
     private String updateId;
